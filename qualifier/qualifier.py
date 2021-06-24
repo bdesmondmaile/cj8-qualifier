27c7c30dd200894a1a740eee0ab294706a0c2a73
Bafokeng Maile <66669327+bdesmondmaile@users.noreply.github.com>
2021-06-24 16:06:22 +0200

    Update qualifier.py
    added box drawing characters

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -9,4 +9,12 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
     :param centered: If the items should be aligned to the center, else they are left aligned.
     :return: A table representing the rows passed in.
     """
-    ...
+    Box_draw_chars = {"vertical_pipe": u'\u2502', "right_top": u'\u2510', "right_bottom": u'\u2518', "left_top": u'\u250C', \
+    "left_bottom": u'\u2514', "horiz_pipe": u'\u2500', "cross": u'\u253c', "top_t": u'\u252c', "bottom_t": u'\u2534', "left_t": u'\u251c', \
+    "right_t": u'\u2524' }
+
+    print(Box_draw_chars["horiz_pipe"]);
+
+def main():
+    make_table([12])
+main()
